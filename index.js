7e754fe3451438341c7a4f6151dc548df4bd70c5
shubham-hac <72845960+shubham-hac@users.noreply.github.com>
2024-07-09 10:35:52 +0530

    port config

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const app = require('./app');
 const db = require('./config/db');
 const usermodel = require('./model/user.model')
+const dotenv = require('dotenv');
 
-const port = 3050;
+dotenv.config({path: ".env"}); 
 
 app.get('/', (req,res)=>{
     res.send("Hello!!")
 });
+const port = process.env.PORT;
 
 app.listen(port,() => {
     console.log(`Server is Listening port http://127.0.0.1:${port} or http://localhost:${port}`);
